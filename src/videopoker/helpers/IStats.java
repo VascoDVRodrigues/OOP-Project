883ba54bb97d8e348025fe72c8e6dc3cc6d4f791
André Silva <andpsilva@gmail.com>
2022-06-20 10:37:53 +0100

    Doc in Stats with interface

--- src/videopoker/helpers/IStats.java
+++ src/videopoker/helpers/IStats.java
@@ -1,5 +1,21 @@
 package videopoker.helpers;
 
 public interface IStats {
+
+    /**
+     * This method is used to increment a specified hand stat.
+     * <p>
+     * The String <b>k</b> is used to identify the hand to be incremented, the
+     * <b>cashback</b> is the amount of credits won and the <b>bet</b> is the amount
+     * of credits betted.
+     * </p>
+     * 
+     * <p>
+     * This method increases the stats, the gains, and updates the player balance.
+     * 
+     * @param k        Short name of the type of hand
+     * @param cashback Amount won by player
+     * @param bet      Amount betted
+     */
     public void addStat(String k, int cashback, int bet);
 }
--- src/videopoker/helpers/Stats.java
+++ src/videopoker/helpers/Stats.java
@@ -50,19 +50,7 @@ public class Stats implements IStats{
     }
 
     /**
-     * This method is used to increment a specified hand stat.
-     * <p>
-     * The String <b>k</b> is used to identify the hand to be incremented, the
-     * <b>cashback</b> is the amount of credits won and the <b>bet</b> is the amount
-     * of credits betted.
-     * </p>
-     * 
-     * <p>
-     * This method increases the stats, the gains, and updates the player balance.
-     * 
-     * @param k        Short name of the type of hand
-     * @param cashback Amount won by player
-     * @param bet      Amount betted
+     * {@inheritDoc}
      */
     public void addStat(String k, int cashback, int bet) {
         nB++;
