eef3dbe3fd4c78a5b4a7985adef2dc6a9184afac
André Silva <andpsilva@gmail.com>
2022-06-20 15:22:22 +0100

    more comments

--- src/videopoker/exceptions/IllegalAmountException.java
+++ src/videopoker/exceptions/IllegalAmountException.java
@@ -5,7 +5,7 @@ package videopoker.exceptions;
 public class IllegalAmountException extends Exception{
 
     /**
-     * @param message
+     * 
      */
     public IllegalAmountException() {
         super("b: illegal amount\n");
