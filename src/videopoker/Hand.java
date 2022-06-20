02c208b12ced353ed0dbadd96c7d1a42401f5b13
André Silva <andpsilva@gmail.com>
2022-06-20 01:42:51 +0100

    Documentation on Hand.java

--- src/videopoker/Hand.java
+++ src/videopoker/Hand.java
@@ -4,9 +4,22 @@ import java.util.ArrayList;
 
 import videopoker.deck.Card;
 
+/**
+ * This class is used to implement the player.
+ */
 public class Hand {
+    //Attribute to save the cards inside the hand
     private ArrayList<Card> cards;
 
+    /**
+     * This is the constructor for the Hand class.
+     * <p>
+     * It takes and ArrayList of Card objects.
+     * </p>
+     * 
+     * @param c     An ArrayList of Card objects
+     * @see videopoker.deck.Card
+     */
     public Hand(ArrayList<Card> c) {
         this.cards = c;
     }
@@ -18,25 +31,61 @@ public class Hand {
         return cards;
     }
 
-    // Adds card to hand
+    /**
+     * Method to add a single Card object to the hand;
+     * <p>
+     * It takes a single Card object;
+     * </p>
+     * 
+     * @param c     The Card object
+     * @see videopoker.deck.Card
+     */
     public void addToHand(Card c) {
         cards.add(c);
     }
 
-    // Adds card to hand in a specific index
+    /**
+     * Method to add a single Card object to the hand to a specific index;
+     * <p>
+     * It takes a single Card object and adds it to the hand to a specific index, may be usefull to later produce pretty prints;
+     * This method is overloaded.
+     * </p>
+     * 
+     * @param index The index where the card should be added
+     * @param c     The Card object
+     * @see videopoker.deck.Card
+     */
     public void addToHand(int index, Card c) {
         cards.add(index, c);
     }
 
-    // Deletes the card at a index
+    /**
+     * Method to remove a single Card object from the hand in a specific index;
+     * <p>
+     * It takes the index of the card to be removed.
+     * </p>
+     * 
+     * @param index The index of the card to be removed
+     */
     public void removeFromHand(int idx) {
         cards.remove(idx);
     }
 
-    // Holds the cards at the specified index
+    /**
+     * Method to hold cards at the given indexes;
+     * <p>
+     * It takes an ArrayList of indexes (Integers) of the cards to be held, and an ArrayList of Card objects with the cards that will replace the dropped ones. 
+     * </p>
+     * 
+     * @param idxs      The ArrayList of indexes of the cards to hold
+     * @param toReplace The ArrayList of Cards that will replace the dropped ones
+     * @see videopoker.deck.Card
+     */
     public void holdCards(ArrayList<Integer> idxs, ArrayList<Card> toReplace) {
         // Remove the cards that are not in those indexes
+        
         int curr;
+        //Calculate first the indexes of the cards to be dropped
         ArrayList<Integer> droppedIdxs = new ArrayList<Integer>();
         for (int i = 0; i < 5; i++) {
             if (idxs.contains(i)) {
@@ -46,16 +95,11 @@ public class Hand {
             }
         }
 
-        // Just removing from the list causes the elements to shift and can produce
-        // wrong results
-
-        // Sorting idxs in descending order and removing them in descending order fixes
-        // this
-        // Collections.sort(droppedIdxs, Collections.reverseOrder());
-
+        //Drop those indexes and replace
         while (!droppedIdxs.isEmpty()) {
             curr = droppedIdxs.remove(0);
             this.cards.remove(curr);
+            //Use the addToHand method with the index, so the new card actually replaces in the same spot
             this.addToHand(curr, toReplace.remove(0));
         }
     }
@@ -71,6 +115,11 @@ public class Hand {
         return str.toString();
     }
 
+    /**
+     * Method to print all cards in the hand withouth the conversion to the real card names.
+     * 
+     * @see videopoker.deck.Card#printUnconverted()
+     */
     public void printUnconverted() {
         for (Card card : cards) {
             card.printUnconverted();
